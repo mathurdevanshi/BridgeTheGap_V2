43aa9d3211fba533be0f772f0a950b9d0af39f06
Jack Frost <albarrankevin97@gmail.com>
2019-07-14 19:57:22 -0700

    added api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -10,26 +10,26 @@ export default {
     // },
 
     getAllAgencyData: () => {
-        return axios.post("https://afternoon-sierra-25981.herokuapp.com/agencies/getAllData");
+        return axios.post("https://afternoon-sierra-25981.herokuapp.com/api/agencies/getAllData");
     },
 
     saveDataToInventory: (data) => {
-        return axios.post("https://afternoon-sierra-25981.herokuapp.com/agencies/insertData", data);
+        return axios.post("https://afternoon-sierra-25981.herokuapp.com/api/agencies/insertData", data);
     },
 
     checkUsersToken: (token) => {
-        return axios.post("https://afternoon-sierra-25981.herokuapp.com/agencies/authorize", token);
+        return axios.post("https://afternoon-sierra-25981.herokuapp.com/api/agencies/authorize", token);
     },
 
     registerClient: (newClient) => {
-        return axios.post("https://afternoon-sierra-25981.herokuapp.com/users/client", newClient);
+        return axios.post("https://afternoon-sierra-25981.herokuapp.com/api/users/client", newClient);
     },
 
     registerUser: (newUser) => {
-        return axios.post("https://afternoon-sierra-25981.herokuapp.com/users/register", newUser);
+        return axios.post("https://afternoon-sierra-25981.herokuapp.com/api/users/register", newUser);
     },
 
     loginUser: (user) => {
-        return axios.post("https://afternoon-sierra-25981.herokuapp.com/users/login", user);
+        return axios.post("https://afternoon-sierra-25981.herokuapp.com/api/users/login", user);
     }
 }
